2553faad6ce03dfd47cd32b11e723264de4a55e2
TomatoPana <mdlb.lobo@gmail.com>
2021-06-13 12:22:40 -0500

    Hemos hecho un chingo de cosas pt. 3

---
+++ app/src/main/java/com/mdlozano/proyectofinal/activities/SucursalesActivity.java
@@ -0,0 +1,15 @@
+package com.mdlozano.proyectofinal.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.mdlozano.proyectofinal.R;
+
+public class SucursalesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sucursales);
+    }
+}
