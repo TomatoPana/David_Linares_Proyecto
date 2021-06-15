3b2a2b29bb43f4554e2527ed73a3a15b35156ada
TomatoPana <mdlb.lobo@gmail.com>
2021-06-14 21:57:08 -0500

    Chingos de cambios de cosas que no sirven

---
+++ app/src/main/java/com/mdlozano/proyectofinal/activities/ArticulosActivity.java
@@ -0,0 +1,14 @@
+package com.mdlozano.proyectofinal.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ArticulosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_articulos);
+    }
+}
---
+++ app/src/main/java/com/mdlozano/proyectofinal/adapters/ArticulosCustomAdapter.java
@@ -0,0 +1,2 @@
+package com.mdlozano.proyectofinal.adapters;public class ArticulosCustomAdapter {
+}
